58437763d9aa8e97108e60df902bbc60cc28fa83
hsahu4012 <hrudayanandasahu3@gmail.com>
2024-02-28 15:47:45 +0530

    Modal Login Page

---
+++ src/components/Modal.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+
+const Modal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
+      <div className="modal-box">
+        <div className="modal-action flex flex-col justify-center mt-0 ">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="card-body"
+            method="dialog"
+          >
+            <h3 className="font-bold text-lg">Please Login!</h3>
+
+            {/* email */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+                {...register("email")}
+              />
+            </div>
+            {/* password */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+                {...register("password")}
+              />
+              <label className="label mt-1">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+
+            {/* Error Text */}
+
+            {/* Login Btn */}
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                value="Login"
+                className="btn bg-green text-white"
+              />
+            </div>
+            {/* Sign Up */}
+            <p className="text-center my-2">
+              Do not have an account?{" "}
+              <Link to="/signup" className="underline text-red ml-1">
+                Signup Now
+              </Link>
+            </p>
+          </form>
+
+          {/* Social Sign In */}
+          <div className="text-center space-x-3 mb-5">
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGoogle />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaFacebookF />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGithub />
+            </button>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import logo from "/logo.png";
-import { BiPhoneCall } from "react-icons/bi";
+import { FaRegUser } from "react-icons/fa";
+import Modal from "./Modal";
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
 
-  // Hamdle Scroll Function
+  // Handle Scroll Function
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
@@ -151,11 +152,15 @@ const Navbar = () => {
             </div>
           </div>
 
-          {/* Button */}
-          <a className="btn bg-green rounded-full px-6 text-white flex items-center  gap-2">
-            <BiPhoneCall />
-            Contact
-          </a>
+          {/* Login Button */}
+          <button
+            onClick={() => document.getElementById("my_modal_5").showModal()}
+            className="btn bg-green rounded-full px-6 text-white flex items-center  gap-2"
+          >
+            <FaRegUser />
+            Login
+          </button>
+          <Modal />
         </div>
       </div>
     </header>
